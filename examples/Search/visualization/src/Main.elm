9fa37b24e39f60d09bfc669ff67e20ed2c4fa9d8
David Pomerenke <david.pomerenke@uni-konstanz.de>
2020-08-03 08:55:59 +0200

    Optimize search visualization, add tooltip
    - optimize using a dictionary of states representing the tree of checked states, and updating it only with new states (that is, child states of the current head of the `explored` list)

--- examples/Search/visualization/src/Main.elm
+++ examples/Search/visualization/src/Main.elm
@@ -2,6 +2,7 @@ module Main exposing (..)
 
 import Browser
 import Browser.Dom exposing (Element)
+import Browser.Events
 import Dict exposing (Dict)
 import Element
     exposing
@@ -12,10 +13,15 @@ import Element
         , column
         , el
         , fill
+        , fillPortion
         , height
+        , html
+        , htmlAttribute
         , layout
+        , mouseOver
         , none
         , padding
+        , pointer
         , rgb
         , row
         , scale
@@ -26,6 +32,10 @@ import Element
         )
 import Element.Background as Background
 import Element.Border as Border
+import Element.Events as Events
+import Html exposing (div)
+import Html.Attributes exposing (style)
+import Json.Decode
 import List.Extra as List
 import Process
 import Search
@@ -36,11 +46,12 @@ import Search
         , Solution(..)
         , graphSearchStep
         , init
+        , insertBy
         , insertFirst
         , insertLast
         , path
         )
-import Search.Problems.NPuzzle exposing (mediumEightPuzzle)
+import Search.Problems.NPuzzle exposing (complexEightPuzzle, mediumEightPuzzle)
 import Task
 
 
@@ -49,33 +60,89 @@ main =
         { view =
             \model ->
                 { title = "Breadth-first search of 8-Puzzle"
-                , body =
-                    [ layout [ scale 0.95 ]
-                        (visualization (\_ -> none) model.searchModel)
-                    ]
+                , body = [ layout [] (body model) ]
                 }
         , init = init
         , update = update
-        , subscriptions = \_ -> Sub.none
+        , subscriptions = \_ -> Browser.Events.onMouseMove (Json.Decode.map Move decode)
         }
+
+
+body : Model -> Element Msg
+body model =
+    column [ width fill, height fill, pointer ]
+        [ model.shown
+            |> Maybe.map
+                (\state ->
+                    column
+                        (List.map htmlAttribute
+                            [ style "position" "absolute"
+                            , style "left" (String.fromFloat (Tuple.first model.pos) ++ "px")
+                            , style "top" (String.fromFloat (Tuple.second model.pos) ++ "px")
+                            , style "z-index" "10"
+                            ]
+                            ++ [ Background.color (rgb 1 1 1)
+                               , padding 20
+                               , spacing 20
+                               , Border.rounded 20
+                               , Border.glow (rgb 0.5 0.5 0.5) 5
+                               ]
+                        )
+                        [ visualizeNPuzzle state
+                        , allNodes model.searchModel
+                            |> List.find (\node -> node.state == state)
+                            |> Maybe.map (path >> List.map (Debug.toString >> text) >> column [])
+                            |> Maybe.withDefault none
+                        ]
+                )
+            |> Maybe.withDefault none
+        , visualizeCache model.visualizationCache
+        ]
+
+
+allNodes : Search.Model state -> List (Node state)
+allNodes model =
+    (case model.solution of
+        Solution a ->
+            [ a ]
+
+        _ ->
+            []
+    )
+        ++ model.explored
+        ++ model.frontier
+
+
+decode : Json.Decode.Decoder ( Float, Float )
+decode =
+    Json.Decode.map2 (\a b -> ( a, b ))
+        (Json.Decode.field "pageX" Json.Decode.float)
+        (Json.Decode.field "pageY" Json.Decode.float)
 
 
 type alias State =
     List Int
 
 
-type alias Cache =
-    Dict State (List State)
+type alias Cache state =
+    { root : state
+    , treeDict : Dict state (List state)
+    }
 
 
 type alias Model =
     { searchModel : Search.Model State
-    , visualizationCache : Cache
+    , visualizationCache : Cache State
+    , shown : Maybe State
+    , pos : ( Float, Float )
     }
 
 
 type Msg
     = NewModel (Search.Model State)
+    | Show State
+    | Hide State
+    | Move ( Float, Float )
 
 
 searchTask : Search.Model State -> Cmd Msg
@@ -98,16 +165,69 @@ init =
     \_ ->
         let
             initialModel =
-                Search.init insertLast mediumEightPuzzle
+                Search.init (insertBy (\node -> node.pathCost + mediumEightPuzzle.heuristic node.state)) mediumEightPuzzle
         in
-        ( { searchModel = initialModel, visualizationCache = Dict.empty }, searchTask initialModel )
+        ( { searchModel = initialModel
+          , visualizationCache =
+                { root = mediumEightPuzzle.initialState
+                , treeDict = Dict.empty
+                }
+          , shown = Nothing
+          , pos = ( 0, 0 )
+          }
+        , searchTask initialModel
+        )
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         NewModel m ->
-            ( { model | searchModel = m }, searchTask m )
+            ( { model
+                | searchModel = m
+                , visualizationCache = updateCache m model.visualizationCache
+              }
+            , searchTask m
+            )
+
+        Show s ->
+            ( { model | shown = Just s }, Cmd.none )
+
+        Hide s ->
+            ( if Just s == model.shown then
+                { model | shown = Nothing }
+
+              else
+                model
+            , Cmd.none
+            )
+
+        Move ( x, y ) ->
+            ( { model | pos = ( x, y ) }, Cmd.none )
+
+
+updateCache : Search.Model State -> Cache State -> Cache State
+updateCache model cache =
+    case model.explored of
+        h :: _ ->
+            { cache
+                | treeDict =
+                    cache.treeDict
+                        |> Dict.insert h.state
+                            (List.map .state (children h model.frontier))
+                        |> (case model.solution of
+                                Solution a ->
+                                    List.head model.frontier
+                                        |> Maybe.map (\node -> Dict.insert node.state [ a.state ])
+                                        |> Maybe.withDefault identity
+
+                                _ ->
+                                    identity
+                           )
+            }
+
+        _ ->
+            cache
 
 
 emptyNode : Node State
@@ -140,8 +260,8 @@ properties level maxLevel =
         s =
             1 - (toFloat level / max (toFloat maxLevel) 5 * 0.4)
     in
-    [ width fill
-    , height fill
+    [ width (fillPortion 80)
+    , height (fillPortion 80)
     , padding 2
     , spacing 2
     , Border.rounded 20
@@ -155,7 +275,7 @@ columnOrRow level maxLevel =
         column
 
      else
-        wrappedRow
+        row
     )
     <|
         properties level maxLevel
@@ -164,7 +284,7 @@ columnOrRow level maxLevel =
 rowOrColumn : Int -> Int -> (List (Element Msg) -> Element Msg)
 rowOrColumn level maxLevel =
     (if modBy 2 level == 0 then
-        wrappedRow
+        row
 
      else
         column
@@ -173,16 +293,47 @@ rowOrColumn level maxLevel =
         properties level maxLevel
 
 
-boxify : (State -> Element Msg) -> Int -> Int -> Hierarchy State -> Element Msg
-boxify visualizeState level maxDepth (Hierarchy ( a, h )) =
+boxify : Int -> Int -> Hierarchy State -> Element Msg
+boxify level maxDepth (Hierarchy ( a, h )) =
     columnOrRow level
         maxDepth
-        (el [ centerX, centerY ] (visualizeState a)
+        (el [ centerX, centerY ] none
             :: (if List.length h > 0 then
                     [ rowOrColumn
                         level
                         maxDepth
-                        (List.map (boxify visualizeState (level + 1) maxDepth) h)
+                        (List.map (boxify (level + 1) maxDepth) h)
+                    ]
+
+                else
+                    []
+               )
+        )
+
+
+boxifyWithCache : Int -> Int -> Dict State (List State) -> State -> Element Msg
+boxifyWithCache level maxDepth cache state =
+    let
+        children_ =
+            Maybe.withDefault [] (Dict.get state cache)
+    in
+    columnOrRow level
+        maxDepth
+        (el
+            [ width (fillPortion 20)
+            , height (fillPortion 20)
+            , Events.onMouseEnter (Show state)
+            , Events.onMouseLeave (Hide state)
+            ]
+            (el [] none)
+            :: (if List.length children_ > 0 then
+                    [ rowOrColumn
+                        level
+                        maxDepth
+                        (List.map
+                            (boxifyWithCache (level + 1) maxDepth cache)
+                            children_
+                        )
                     ]
 
                 else
@@ -198,7 +349,7 @@ depth l =
 
 visualizeNPuzzle : State -> Element Msg
 visualizeNPuzzle state =
-    column [ scale 0.6 ]
+    column []
         (state
             |> List.groupsOf 3
             |> List.map
@@ -210,10 +361,15 @@ visualizeNPuzzle state =
         )
 
 
-visualization : (State -> Element Msg) -> Search.Model State -> Element Msg
-visualization visualizeState model =
+visualization : Search.Model State -> Element Msg
+visualization model =
     let
         l =
-            model.explored ++ model.frontier
+            allNodes model
     in
-    boxify visualizeState 0 (depth l) (descendants (root l) l)
+    boxify 0 (depth l) (descendants (root l) l)
+
+
+visualizeCache : Cache State -> Element Msg
+visualizeCache cache =
+    boxifyWithCache 0 5 cache.treeDict cache.root
